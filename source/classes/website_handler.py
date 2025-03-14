07c2427fcdf7c2775cd40ce8b5e32f74068981e5
Brian Weisemann <brianw7693@gmail.com>
2025-03-14 15:50:43 -0300

    feat(website_handler): add 'request_get' method

--- source/classes/website_handler.py
+++ source/classes/website_handler.py
@@ -1,16 +1,22 @@
-from playwright.async_api import async_playwright
+from playwright.async_api import async_playwright, APIResponse
 
 
 class WebsiteHandler:
     def __init__(self, headless: bool = True):
         self.headless = headless
         self.page = None
+        self.api_request_context = None
 
     async def initialize_playwright(self):
         playwright = await async_playwright().start()
         browser = playwright.chromium
         browser_context = await browser.launch_persistent_context('', headless=self.headless)
+        self.api_request_context = browser_context.request
         self.page = browser_context.pages[0]
+
+    async def request_get(self, url: str) -> APIResponse:
+        response = await self.api_request_context.get(url)
+        return response
 
     async def destroy(self):
         if self.page is not None:
--- source/tests/website_handler_test.py
+++ source/tests/website_handler_test.py
@@ -1,4 +1,6 @@
-import pytest_asyncio
+from typing import Optional
+
+import pytest
 from playwright.async_api import Page
 
 from source.classes.website_handler import WebsiteHandler
@@ -19,3 +21,38 @@ async def test_initialized_playwright_success(new_instance: WebsiteHandler):
 async def test_page_success(new_instance: WebsiteHandler):
     assert isinstance(new_instance.page, Page)
 
+
+@pytest.mark.parametrize('request_url,response_status', [
+    pytest.param('https://www.pagina12.com.ar/robots.txt', 200),
+])
+async def test_request_get_status_success(request_url: str, response_status: int):
+    wshandler = WebsiteHandler()
+    await wshandler.initialize_playwright()
+    response = await wshandler.request_get(url=request_url)
+    status = response.status
+    assert status == response_status
+
+
+@pytest.mark.parametrize('request_url,response_body', [
+    pytest.param('https://www.pagina12.com.ar/robots.txt', """\
+# robots.txt for https://www.pagina12.com.ar/
+
+Sitemap: https://www.pagina12.com.ar/sitemap.xml.gz
+Sitemap: https://www.pagina12.com.ar/breakingnews.xml
+Sitemap: https://www.pagina12.com.ar/breakingnews-short.xml
+
+User-agent: *
+Disallow: /logout-user?redirect=*
+Disallow: /349353471/
+Disallow: /admin/
+Disallow: /andytow/
+Disallow: /apps/talk
+Disallow: /apps/zar
+"""),
+])
+async def test_request_get_body_success(request_url: str, response_body: Optional[str]):
+    wshandler = WebsiteHandler()
+    await wshandler.initialize_playwright()
+    response = await wshandler.request_get(url=request_url)
+    text = await response.text()
+    assert text == response_body
