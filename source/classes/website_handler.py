6b5cf4ad848df7cb4c44ddf2c73c2a2d54195eb0
Brian Weisemann <brianw7693@gmail.com>
2025-03-14 12:55:39 -0300

    feat(website_handler): add 'page' property
    Make 'destroy' method asynchronous and adapt 'new_instance' fixture
    accordingly.

--- source/classes/website_handler.py
+++ source/classes/website_handler.py
@@ -1,4 +1,24 @@
-class WebsiteHandler:
+import sys
+import traceback
 
-    def destroy(self):
-        pass
+from playwright.async_api import async_playwright
+
+
+class WebsiteHandler:
+    def __init__(self,
+                 ):
+        self.page = None
+
+    async def initialize_playwright(self):
+        try:
+            playwright = await async_playwright().start()
+            browser = playwright.chromium
+            browser_context = await browser.launch_persistent_context('', headless=False)
+            self.page = browser_context.pages[0]
+        except:
+            print(''.join(traceback.format_exception(*sys.exc_info())))
+
+    async def destroy(self):
+        if self.page is not None:
+            await self.page.close()
+            self.page = None
--- source/tests/website_handler_test.py
+++ source/tests/website_handler_test.py
@@ -1,14 +1,29 @@
-import pytest
+import pytest_asyncio
+from playwright.async_api import Page
 
 from source.classes.website_handler import WebsiteHandler
 
 
-@pytest.fixture
-def new_instance() -> WebsiteHandler:
+@pytest_asyncio.fixture(scope='module')
+async def new_instance() -> WebsiteHandler:
     wshandler = WebsiteHandler()
     yield wshandler
-    wshandler.destroy()
+    await wshandler.destroy()
+
+
+@pytest_asyncio.fixture(scope='module')
+async def initialized_playwright(new_instance) -> WebsiteHandler:
+    await new_instance.initialize_playwright()
+    return new_instance
 
 
 def test_instantiation_success(new_instance: WebsiteHandler):
     assert isinstance(new_instance, WebsiteHandler)
+
+
+async def test_initialized_playwright_success(initialized_playwright: WebsiteHandler):
+    assert isinstance(initialized_playwright, WebsiteHandler)
+
+
+async def test_page(initialized_playwright: WebsiteHandler):
+    assert isinstance(initialized_playwright.page, Page)
