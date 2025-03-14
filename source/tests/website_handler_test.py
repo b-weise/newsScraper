055e6a1c6a50e2e57973e700839bb53541eb2453
Brian Weisemann <brianw7693@gmail.com>
2025-03-14 15:40:46 -0300

    refactor(website_handler tests): remove 'initialized_playwright' fixture
    Make 'new_instance' initialize Playwright. Remove the
    'test_instantiation_success' test case. Adapt other test cases
    accordingly. Replace 'pytest_asyncio' decorators with 'pytest', as
    'asyncio_mode' is set to 'auto' in pytest.ini.

--- source/tests/website_handler_test.py
+++ source/tests/website_handler_test.py
@@ -4,26 +4,18 @@ from playwright.async_api import Page
 from source.classes.website_handler import WebsiteHandler
 
 
-@pytest_asyncio.fixture(scope='module')
+@pytest.fixture(scope='module')
 async def new_instance() -> WebsiteHandler:
     wshandler = WebsiteHandler()
+    await wshandler.initialize_playwright()
     yield wshandler
     await wshandler.destroy()
 
 
-@pytest_asyncio.fixture(scope='module')
-async def initialized_playwright(new_instance) -> WebsiteHandler:
-    await new_instance.initialize_playwright()
-    return new_instance
-
-
-def test_instantiation_success(new_instance: WebsiteHandler):
+async def test_initialized_playwright_success(new_instance: WebsiteHandler):
     assert isinstance(new_instance, WebsiteHandler)
 
 
-async def test_initialized_playwright_success(initialized_playwright: WebsiteHandler):
-    assert isinstance(initialized_playwright, WebsiteHandler)
+async def test_page_success(new_instance: WebsiteHandler):
+    assert isinstance(new_instance.page, Page)
 
-
-async def test_page_success(initialized_playwright: WebsiteHandler):
-    assert isinstance(initialized_playwright.page, Page)
