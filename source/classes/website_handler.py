717b8994ff5712c711346a811e014ab36538e0b5
Brian Weisemann <brianw7693@gmail.com>
2025-03-14 15:42:41 -0300

    feat(website_handler): add 'headless' as constructor parameter
    Clean up unused imports.

--- source/classes/website_handler.py
+++ source/classes/website_handler.py
@@ -1,18 +1,15 @@
-import sys
-import traceback
-
 from playwright.async_api import async_playwright
 
 
 class WebsiteHandler:
-    def __init__(self,
-                 ):
+    def __init__(self, headless: bool = True):
+        self.headless = headless
         self.page = None
 
     async def initialize_playwright(self):
         playwright = await async_playwright().start()
         browser = playwright.chromium
-        browser_context = await browser.launch_persistent_context('', headless=False)
+        browser_context = await browser.launch_persistent_context('', headless=self.headless)
         self.page = browser_context.pages[0]
 
     async def destroy(self):
