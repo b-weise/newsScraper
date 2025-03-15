1df2669e75b89db7c176bc4a088e107378105305
Brian Weisemann <brianw7693@gmail.com>
2025-03-15 18:35:06 -0300

    refactor(website_handler tests): add 'new_initialized_instance' fixture
    Adapt the rest of the tests and fixtures accordingly

--- source/tests/website_handler_test.py
+++ source/tests/website_handler_test.py
@@ -7,29 +7,33 @@ from playwright.async_api import Page
 from source.classes.website_handler import WebsiteHandler
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture
 async def new_instance() -> WebsiteHandler:
     wshandler = WebsiteHandler()
-    await wshandler.initialize_playwright()
     yield wshandler
     await wshandler.destroy()
+
+
+@pytest.fixture
+async def new_initialized_instance(new_instance: WebsiteHandler) -> WebsiteHandler:
+    await new_instance.initialize_playwright()
+    return new_instance
 
 
 async def test_initialized_playwright_success(new_instance: WebsiteHandler):
     assert isinstance(new_instance, WebsiteHandler)
 
 
-async def test_page_success(new_instance: WebsiteHandler):
-    assert isinstance(new_instance.page, Page)
+async def test_page_success(new_initialized_instance: WebsiteHandler):
+    assert isinstance(new_initialized_instance.page, Page)
 
 
 @pytest.mark.parametrize('request_url,response_status', [
     pytest.param('https://www.pagina12.com.ar/robots.txt', 200),
 ])
-async def test_request_get_status_success(request_url: str, response_status: int):
-    wshandler = WebsiteHandler()
-    await wshandler.initialize_playwright()
-    response = await wshandler.request_get(url=request_url)
+async def test_request_get_status_success(new_initialized_instance: WebsiteHandler, request_url: str,
+                                          response_status: int):
+    response = await new_initialized_instance.request_get(request_url)
     status = response.status
     assert status == response_status
 
@@ -51,10 +55,9 @@ Disallow: /apps/talk
 Disallow: /apps/zar
 """),
 ])
-async def test_request_get_body_success(request_url: str, response_body: Optional[str]):
-    wshandler = WebsiteHandler()
-    await wshandler.initialize_playwright()
-    response = await wshandler.request_get(url=request_url)
+async def test_request_get_body_success(new_initialized_instance: WebsiteHandler, request_url: str,
+                                        response_body: Optional[str]):
+    response = await new_initialized_instance.request_get(request_url)
     text = await response.text()
     assert text == response_body
 
@@ -120,9 +123,7 @@ async def test_parse_robots_file_success(new_instance, input_content: str, expec
     pytest.param('https://www.pagina12.com.ar/test/', {'allow': ['/*/*/'], 'disallow': ['/*/']}, False),
     pytest.param('https://www.pagina12.com.ar/test/ing/', {'allow': ['/*/*/'], 'disallow': ['/*/']}, True),
 ])
-async def test_is_compliant_url_success(new_instance,
-                                        input_url: str,
-                                        parsed_robots: dict[str, list[str]],
+async def test_is_compliant_url_success(new_instance, input_url: str, parsed_robots: dict[str, list[str]],
                                         expected_output: bool):
     is_compliant = new_instance.is_compliant_url(input_url, parsed_robots)
     assert is_compliant == expected_output
