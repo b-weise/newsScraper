3bd0088afdee2654cb96f3038cbfd468cab40d63
Brian Weisemann <brianw7693@gmail.com>
2025-03-17 16:49:03 -0300

    refactor(website_handler): make internal properties private

--- source/classes/website_handler.py
+++ source/classes/website_handler.py
@@ -21,13 +21,13 @@ class WebsiteHandler:
     def __init__(self, headless: bool = True, robots_useragent_key: str = 'user-agent',
                  robots_allow_key: str = 'allow', robots_disallow_key: str = 'disallow',
                  default_timeout: int = 5, default_navigation_timeout: int = 15):
-        self.headless = headless
-        self.robots_useragent_key = robots_useragent_key
-        self.robots_allow_key = robots_allow_key
-        self.robots_disallow_key = robots_disallow_key
+        self.__headless = headless
+        self.__robots_useragent_key = robots_useragent_key
+        self.__robots_allow_key = robots_allow_key
+        self.__robots_disallow_key = robots_disallow_key
         self.__default_timeout = default_timeout
         self.__default_navigation_timeout = default_navigation_timeout
-        self.common_useragents_url = 'https://www.useragents.me/'
+        self.__common_useragents_url = 'https://www.useragents.me/'
         self.__page = None
         self.__browser_context = None
         self.__parsed_robots = None
@@ -36,7 +36,7 @@ class WebsiteHandler:
         await self.destroy()
         playwright = await async_playwright().start()
         browser = playwright.chromium
-        self.__browser_context = await browser.launch_persistent_context('', headless=self.headless,
+        self.__browser_context = await browser.launch_persistent_context('', headless=self.__headless,
                                                                          user_agent=user_agent)
         self.__page = self.__browser_context.pages[0]
         self.__setup_page(self.__page)
@@ -67,9 +67,9 @@ class WebsiteHandler:
         def is_keyword_present(line: str, keyword: str) -> bool:
             return re.match(f'^{keyword}', line, flags=re.I) is not None
 
-        useragent_key = self.robots_useragent_key
-        allow_key = self.robots_allow_key
-        disallow_key = self.robots_disallow_key
+        useragent_key = self.__robots_useragent_key
+        allow_key = self.__robots_allow_key
+        disallow_key = self.__robots_disallow_key
         parsed_content = {
             f'{allow_key}': [],
             f'{disallow_key}': [],
@@ -96,8 +96,8 @@ class WebsiteHandler:
     def is_compliant_url(self, url: str, parsed_robots: Optional[dict[str, list[str]]] = None) -> bool:
         # criteria based on: https://developers.google.com/search/docs/crawling-indexing/robots/create-robots-txt
         parsed_robots = parsed_robots or {}
-        allowed_paths = parsed_robots.get(self.robots_allow_key, [])
-        disallowed_paths = parsed_robots.get(self.robots_disallow_key, [])
+        allowed_paths = parsed_robots.get(self.__robots_allow_key, [])
+        disallowed_paths = parsed_robots.get(self.__robots_disallow_key, [])
         url_path = urlparse(url).path
         all_paths = allowed_paths + disallowed_paths
         all_paths_sorted = sorted(all_paths,
@@ -122,7 +122,7 @@ class WebsiteHandler:
 
     async def get_common_useragent(self) -> str:
         self.__check_playwright_instance()
-        await self.__page.goto(self.common_useragents_url)
+        await self.__page.goto(self.__common_useragents_url)
         json_parent_div = self.__page.locator('#most-common-desktop-useragents-json-csv')
         json_div = json_parent_div.locator('div', has_text='JSON')
         json_textarea = json_div.locator('textarea')
